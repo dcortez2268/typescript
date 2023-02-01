e4a6b2d746db4bd4cf8a56b5d2364f9d0ca44da2
Dominick Cortez <jdcortez2268@gmail.com>
2023-02-01 12:02:09 -0800

    global app state w singleton and sub patterns

--- dragAndDrop/app.js
+++ dragAndDrop/app.js
@@ -1,10 +1,47 @@
 "use strict";
+//
+//
+// Project Type
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+//
+//
+// PROJECT STATE MANAGEMENT CLASS
+class ProjectState {
+    constructor() {
+        this.listeners = [];
+        this.projects = [];
+    }
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        else {
+            this.instance = new ProjectState();
+            return this.instance;
+        }
+    }
+    addProject(title, description, people) {
+        const newProject = {
+            id: Math.random().toString(),
+            title,
+            description,
+            people,
+        };
+        this.projects.push(newProject);
+        for (const listenerFn of this.listeners) {
+            listenerFn(this.projects.slice()); // slice method returns shallow copy of array
+        }
+    }
+    addListener(listenerFn) {
+        this.listeners.push(listenerFn);
+    }
+}
+const projectState = ProjectState.getInstance();
 function validate(input) {
     let isValid = true;
     if (input.required) {
@@ -26,7 +63,7 @@ function validate(input) {
 }
 //
 //
-// autobind decorator
+// AUTOBIND DECORATOR
 function Autobind(target, methodName, descriptor) {
     const originalMethod = descriptor.value;
     const adjDescriptor = {
@@ -41,7 +78,44 @@ function Autobind(target, methodName, descriptor) {
 }
 //
 //
-// ProjectInput Class
+// ProjectList CLASS
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.assignedProjects = [];
+        this.templateElement = document.getElementById('project-list');
+        this.app = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        projectState.addListener((projects) => {
+            this.assignedProjects = projects;
+            this.renderProjects();
+        });
+        this.attach();
+        this.renderContent();
+    }
+    renderProjects() {
+        const listEl = document.getElementById(`${this.type}-projects-list`);
+        listEl.textContent = '';
+        for (const project of this.assignedProjects) {
+            const listItem = document.createElement('li');
+            listItem.textContent = project.title;
+            listEl.appendChild(listItem);
+        }
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent = `${this.type.toUpperCase()} PROJECTS`;
+    }
+    attach() {
+        this.app.insertAdjacentElement('afterend', this.element);
+    }
+}
+//
+//
+// ProjectInput CLASS
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
@@ -58,9 +132,10 @@ class ProjectInput {
     submitHandler(e) {
         e.preventDefault();
         const userInput = this.gatherUserInput();
+        // if valid input is given, userInput will be tuple
         if (Array.isArray(userInput)) {
             const [title, desc, people] = userInput;
-            console.log(title, desc, people);
+            projectState.addProject(title, desc, people);
         }
         this.clearInputs();
     }
@@ -107,4 +182,5 @@ __decorate([
     Autobind
 ], ProjectInput.prototype, "submitHandler", null);
 const projInput = new ProjectInput();
-console.log('hello');
+const finishedProjectList = new ProjectList('finished');
+const activeProjectList = new ProjectList('active');
