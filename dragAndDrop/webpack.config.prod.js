1a0783bbdf3fb768c1875bb8551a224a860217ed
Dominick Cortez <jdcortez2268@gmail.com>
2023-02-21 20:10:50 -0800

    finishing the setup, adding dev and prod workflow

--- dragAndDrop/webpack.config.prod.js
+++ dragAndDrop/webpack.config.prod.js
@@ -1,19 +1,12 @@
 const path = require('path')
+const CleanPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/app.ts',
-    devServer: {
-        static: [
-            {
-                directory: path.join(__dirname),
-            },
-        ],
-    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
     },
     module: {
         rules: [
@@ -27,4 +20,5 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.js'],
     },
+    plugins: [new CleanPlugin.CleanWebpackPlugin()],
 }
